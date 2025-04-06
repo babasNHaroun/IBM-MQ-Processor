ea98852613c00eef65132d1ad5c6000351f5af24
saidanir <tamouhamb@gmail.com>
2025-04-06 15:04:08 +0200

    Updated Message and Partner entites

--- mqManager-backend/src/main/java/com/mq/manager/mqManager/entity/Message.java
+++ mqManager-backend/src/main/java/com/mq/manager/mqManager/entity/Message.java
@@ -2,19 +2,31 @@ package com.mq.manager.mqManager.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "messages")
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "received_at", nullable = false)
     private LocalDateTime receivedAt;
+
+    @Column(name = "source_application", nullable = false)
     private String sourceApplication;
 }
--- mqManager-backend/src/main/java/com/mq/manager/mqManager/entity/Partner.java
+++ mqManager-backend/src/main/java/com/mq/manager/mqManager/entity/Partner.java
@@ -3,30 +3,40 @@ package com.mq.manager.mqManager.entity;
 import com.mq.manager.mqManager.Utils.enums.Direction;
 import com.mq.manager.mqManager.Utils.enums.ProcessedFlowType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NonNull;
 
-@Entity
 @Data
+@Entity
+@Table(name = "partners")
 public class Partner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NonNull
+    @NotBlank(message = "alias is required")
+    @Column(nullable = false)
     private String alias;
 
-    @NonNull
+    @NotBlank(message = "Type is required")
+    @Column(nullable = false)
     private String type;
 
+    @NotNull(message = "Direction is required")
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Direction direction;
 
     private String application;
 
+    @NotNull(message = "Processed flow type is required")
     @Enumerated(EnumType.STRING)
-    private ProcessedFlowType flowType;
+    @Column(name = "processed_flow_type", nullable = false)
+    private ProcessedFlowType processedFlowType;
 
-    @NonNull
+    @NotBlank(message = "Description is required")
+    @Column(nullable = false)
     private String description;
 }
