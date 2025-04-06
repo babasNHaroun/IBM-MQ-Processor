830a3e7349064b8587ebd52e82055773ff7dd9a8
saidanir <tamouhamb@gmail.com>
2025-04-07 00:43:32 +0200

    updated MQConfig class after spring boot version update + added user/password to connect to the local IBM MQ

--- mqManager-backend/src/main/java/com/mq/manager/mqManager/config/MQConfig.java
+++ mqManager-backend/src/main/java/com/mq/manager/mqManager/config/MQConfig.java
@@ -1,14 +1,20 @@
 package com.mq.manager.mqManager.config;
 
 import com.ibm.mq.jms.MQQueueConnectionFactory;
+import com.ibm.msg.client.wmq.WMQConstants;
+
+import javax.jms.ConnectionFactory;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Configuration
+@EnableJms
 public class MQConfig {
     private static final Logger logger = LoggerFactory.getLogger(MQConfig.class);
 
@@ -24,19 +30,33 @@ public class MQConfig {
     @Value("${ibm.mq.channel}")
     private String channel;
 
+    @Value("${ibm.mq.user}")
+    private String user;
+
+    @Value("${ibm.mq.password}")
+    private String password;
+
     @Bean
-    public MQQueueConnectionFactory connectionFactory() {
+    public ConnectionFactory connectionFactory() {
         MQQueueConnectionFactory factory = new MQQueueConnectionFactory();
         try {
             factory.setHostName(host);
             factory.setPort(port);
             factory.setQueueManager(queueManager);
             factory.setChannel(channel);
+            factory.setTransportType(WMQConstants.WMQ_CM_CLIENT);
+            factory.setStringProperty(WMQConstants.USERID, user);
+            factory.setStringProperty(WMQConstants.PASSWORD, password);
 
         } catch (javax.jms.JMSException e) {
             logger.error("Failed to configure MQQueueConnectionFactory", e);
             throw new RuntimeException("MQ configuration failed", e);
         }
         return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        return new JmsTemplate(connectionFactory);
     }
 }
