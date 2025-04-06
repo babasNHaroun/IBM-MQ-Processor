4b88068debf4b2b92aff118ea9c016c112ce0a63
saidanir <tamouhamb@gmail.com>
2025-04-06 15:36:18 +0200

    mqManager-backend/

--- mqManager-backend/src/main/java/com/mq/manager/mqManager/config/MQConfig.java
+++ mqManager-backend/src/main/java/com/mq/manager/mqManager/config/MQConfig.java
@@ -1,6 +1,8 @@
 package com.mq.manager.mqManager.config;
 
 import com.ibm.mq.jms.MQQueueConnectionFactory;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.slf4j.Logger;
@@ -10,14 +12,26 @@ import org.slf4j.LoggerFactory;
 public class MQConfig {
     private static final Logger logger = LoggerFactory.getLogger(MQConfig.class);
 
+    @Value("${ibm.mq.host}")
+    private String host;
+
+    @Value("${ibm.mq.port}")
+    private Integer port;
+
+    @Value("${ibm.mq.queueManager}")
+    private String queueManager;
+
+    @Value("${ibm.mq.channel}")
+    private String channel;
+
     @Bean
     public MQQueueConnectionFactory connectionFactory() {
         MQQueueConnectionFactory factory = new MQQueueConnectionFactory();
         try {
-            factory.setHostName("localhost");
-            factory.setPort(1414);
-            factory.setQueueManager("QM1");
-            factory.setChannel("DEV.APP.SVRCONN");
+            factory.setHostName(host);
+            factory.setPort(port);
+            factory.setQueueManager(queueManager);
+            factory.setChannel(channel);
 
         } catch (javax.jms.JMSException e) {
             logger.error("Failed to configure MQQueueConnectionFactory", e);
